4117351931a1ed666cd2498385acd7138be40fe2
carlos <carl0s@bu.edu>
2015-11-30 13:32:07 -0500

    popups, threading, copying

--- ithc_updater/updater.py
+++ ithc_updater/updater.py
@@ -1,8 +1,79 @@
 #!/usr/bin/python
 import sys, subprocess, os
 from PyQt5.QtWidgets import QWidget, QCheckBox, QApplication, QPushButton, QFileDialog, QMainWindow, QAction, QMessageBox
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, QThread
+from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon, QProgressBar, QGridLayout
+import shutil
+
+class CopyThread(QThread):
+    procDone = QtCore.pyqtSignal(bool)
+    partDone = QtCore.pyqtSignal(int)
+    
+    def __init__(self, src, dest):
+        super(CopyThread, self).__init__(self)
+        self.src = src
+        self.dest = dest
+        
+    def run(self):
+        makedirs(dest)
+        numCopied = 0
+        for path, dirs, filenames in os.walk(src):
+            for directory in dirs:
+                destDir = path.replace(src,dest)
+                makedirs(os.path.join(destDir, directory))
+                
+            for sfile in filenames:
+                srcFile = os.path.join(path, sfile)
+                destFile = os.path.join(path.replace(src, dest), sfile)
+                shutil.copy(srcFile, destFile)
+                numCopied += 1
+                self.partDone.emit(numCopied)
+        self.procDone.emit(True)
+
+class CopyProgressPopup(QWidget):
+    def __init__(self, src, dest):
+        super(CopyProgressPopup, self).__init__(self)
+        self.src = src
+        self.dest = dest
+        self.numFiles = countFiles(src)
+        self.thread = CopyThread(src, dest)
+        
+        self.progressbar = QProgressBar()
+        self.progressbar.setMinimum(0)
+        self.progressbar.setMaxiumum(self.numFiles)
+        
+        self.progresslabel = QLabel("0.0%")
+        
+        self.setWindowTitle("Copying to " + dest)
+        
+        self.layout = QGridLayout()
+        layout.addWidget(self.progressbar, 0, 0)
+        layout.addWidget(self.progresslabel, 0, 1)
+        
+        self.setLayout(layout)
+        
+        self.thread.partDone.connect(self.update)
+        self.thread.procDone.connect(self.finish)
+        
+        self.show()
+        self.thread.start()
+        
+    def update(self, val):
+        self.progressbar.setValue(val/self.numFiles)
+        perct = "{0}%".format(val/self.numFiles)
+        self.nameLabel.setText(perct)
+        
+    def finish(self):
+        self.layout.removeWidget(self.progressbar)
+        self.layout.removeWidget(self.progresslabel)
+        self.progressbar.deleteLater()
+        self.progresslable.deleteLater()
+        statuslabel = QLabel("Copy Finished")
+        okayBut = QPushButton("Okay")
+        self.layout.addWidget(statuslabel, 0, 0)
+        self.layout.addWidget(okayBut, 1, 0)
+        okayBut.clicked.connect(close(self))
 
 class DriveButton(QPushButton):
     path = ""
@@ -14,7 +85,7 @@ class DriveButton(QPushButton):
 class Updater(QMainWindow):
     buttons = []
     copyDirectory = ""
-    folders = []
+    pathsToCopyTo = []
     def __init__(self):
         super(Updater, self).__init__()
         self.initUI()
@@ -31,7 +102,6 @@ class Updater(QMainWindow):
         self.toolbar.addAction(fileBrowser)
         self.toolbar.addAction(startCopy)
         for dirs in self.getDrives():
-            self.folders.append(dirs)
             temp = DriveButton(dirs, self.getFullPath(dirs), self)
             temp.setCheckable(True)
             temp.move(10, 40*(len(self.buttons) + 1))
@@ -46,9 +116,9 @@ class Updater(QMainWindow):
         self.statusBar().showMessage("Directory to copy: " + self.copyDirectory)
 
     def startCopyProcedure(self):
-        if copyDirectory:
-            infoBox = QMessageBox(self)
-            infoBox.setText("hello")
+        if (copyDirectory and pathsToCopyTo.len > 0):
+            for dest in pathsToCopyTo:
+                popup = CopyProgressPopup(self.copyDirectory, dest)
     
     def buttonChecked(self):
         sender = self.sender()
